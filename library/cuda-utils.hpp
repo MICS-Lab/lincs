93d2bdb10e4a9116ecddcc279a0b819374941c23
Vincent Jacques <vincent@vincent-jacques.net>
2021-11-21 08:15:22 +0100

    Make CUDA utils more flexible and robust

--- library/cuda-utils.hpp
+++ library/cuda-utils.hpp
@@ -3,34 +3,58 @@
 #ifndef CUDA_UTILS_HPP_
 #define CUDA_UTILS_HPP_
 
+#include <cassert>
+
+
+inline void checkCudaErrors_(const char* file, int line) {
+  cudaError_t error = cudaGetLastError();
+  if (error) {
+    printf(
+      "CUDA ERROR, detected at %s:%i: %i %s\n",
+      file, line, static_cast<unsigned int>(error), cudaGetErrorName(error));
+    assert(false);  // Dump core for further investigations
+  }
+}
+
+#define checkCudaErrors() checkCudaErrors_(__FILE__, __LINE__)
 
 template<typename T>
 T* alloc_host(int n) {
+  if (n == 0) return nullptr;
   T* p;
   cudaMallocHost(&p, n * sizeof(T));
+  checkCudaErrors();
   return p;
 }
 
 template<typename T>
 void free_host(T* p) {
+  if (p == nullptr) return;
   cudaFreeHost(p);
+  checkCudaErrors();
 }
 
 template<typename T>
 T* alloc_device(int n) {
+  if (n == 0) return nullptr;
   T* p;
   cudaMalloc(&p, n * sizeof(T));
+  checkCudaErrors();
   return p;
 }
 
 template<typename T>
 void free_device(T* p) {
+  if (p == nullptr) return;
   cudaFree(p);
+  checkCudaErrors();
 }
 
 template<typename T>
 void copy_host_to_device(int n, const T* src, T* dst) {
+  if (n == 0) return;
   cudaMemcpy(dst, src, n * sizeof(T), cudaMemcpyHostToDevice);
+  checkCudaErrors();
 }
 
 template<typename T>
@@ -42,7 +66,9 @@ T* clone_host_to_device(int n, const T* src) {
 
 template<typename T>
 void copy_device_to_host(int n, const T* src, T* dst) {
+  if (n == 0) return;
   cudaMemcpy(dst, src, n * sizeof(T), cudaMemcpyDeviceToHost);
+  checkCudaErrors();
 }
 
 template<typename T>
