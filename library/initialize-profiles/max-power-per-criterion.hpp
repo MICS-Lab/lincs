b952f8c1a3baacc0d2453eb306e979579cfc71c8
Vincent Jacques <vincent@vincent-jacques.net>
2022-01-06 16:21:57 +0100

    Remove unused, future piece of code

--- library/initialize-profiles/max-power-per-criterion.hpp
+++ library/initialize-profiles/max-power-per-criterion.hpp
@@ -28,16 +28,6 @@ class InitializeProfilesForProbabilisticMaximalDiscriminationPowerPerCriterion :
   std::vector<std::vector<ProbabilityWeightedGenerator<float>>> _generators;
 };
 
-class InitializeProfilesForDeterministicMaximalDiscriminationPowerPerCriterion : public ProfilesInitializationStrategy {
- public:
-  void initialize_profiles(
-    RandomNumberGenerator random,
-    Models<Host>* models,
-    uint iteration_index,
-    std::vector<uint>::const_iterator model_indexes_begin,
-    std::vector<uint>::const_iterator model_indexes_end) override;
-};
-
 }  // namespace ppl
 
 #endif  // INITIALIZE_PROFILES_MAX_POWER_PER_CRITERION_HPP_
