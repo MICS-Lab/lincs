49fe1795e1c2c527d271527bb1b32df3e97e8c59
Vincent Jacques <vincent@vincent-jacques.net>
2022-01-06 14:21:29 +0100

    Fix dependency graph for nested source files

--- builder/deps-to-dot.py
+++ builder/deps-to-dot.py
@@ -5,34 +5,61 @@ import sys
 
 
 print("digraph G {")
+print('  rankdir="LR";')
 print('  node [shape="box"];')
+
 
 def is_important(name):
     if name in {"cuda-utils", "matrix-view", "uint"}:
         return False
     if name.endswith("-tests"):
         return False
-    if name.startswith("test-"):
-        return False
-    if name.startswith("generate-"):
+    if name.startswith("tools/generate-"):
         return False
     return True
 
+
+def normalize_names(names):
+    def gen():
+        for name in names.split():
+            yield os.path.splitext(remove_prefixes(os.path.normpath(name), ["build/obj/", "build/deps/", "library/"]))[0]
+
+    return sorted(set(gen()))
+
+
+def remove_prefixes(s, prefixes):
+    for prefix in prefixes:
+        if s.startswith(prefix):
+            s = s[len(prefix):]
+    return s
+
+all_names = set()
+
 for line in sys.stdin:
-    (left, right) = line.strip().split(":")
+    (left_names, right_names) = map(normalize_names, line.strip().split(":"))
 
-    for right in sorted(set(os.path.splitext(os.path.basename(name))[0] for name in right.split())):
-        if is_important(right):
-            print(f'  "{right}" [color="black", fontcolor="black"];')
+    all_names.update(left_names)
+    all_names.update(right_names)
+
+    for right_name in right_names:
+        if is_important(right_name):
+            print(f'  "{right_name}" [color="black", fontcolor="black"];')
         else:
-            print(f'  "{right}" [color="darkgrey", fontcolor="darkgrey"];')
+            print(f'  "{right_name}" [color="gray40", fontcolor="gray40"];')
 
-        for left in sorted(set(os.path.splitext(os.path.basename(name))[0] for name in left.split())):
-            if left != right:
-                if is_important(left) and is_important(right):
+        for left_name in left_names:
+            if left_name != right_name:
+                if is_important(left_name) and is_important(right_name):
                     attributes = ' [color="black"]'
                 else:
-                    attributes = ' [color="darkgrey"]'
-                print(f'  "{left}" -> "{right}"{attributes};')
+                    attributes = ' [color="gray40"]'
+                print(f'  "{left_name}" -> "{right_name}"{attributes};')
+
+print('  subgraph {')
+print('    rank="same";')
+for name in sorted(all_names):
+    if name.startswith("tools/"):
+        print(f'    "{name}";')
+print("  }")
 
 print("}")
