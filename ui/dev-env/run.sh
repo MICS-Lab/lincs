76d673864c55bce1624f460e4fae0ad5dfe93401
Vincent Jacques <vincent@vincent-jacques.net>
2022-06-07 16:33:56 +0200

    Get started

---
+++ ui/dev-env/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+
+export PPL_FANOUT_PORT=${PPL_FANOUT_PORT:-'8080'}
+export COMPOSE_PROJECT_NAME=ppl-dev
+
+echo "Dev env is launching on http://localhost:$PPL_FANOUT_PORT. Stop it with Ctrl+C"
+
+docker-compose up --build --remove-orphans
+
+# Remove containers and network, keep volumes
+docker-compose down --remove-orphans
