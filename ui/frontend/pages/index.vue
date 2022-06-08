37f8b15824976b6b4d08272e547772951a14b53c
Vincent Jacques <vincent@vincent-jacques.net>
2022-06-08 06:40:17 +0200

    Let the frontend use the backend's API

--- ui/frontend/pages/index.vue
+++ ui/frontend/pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Hello, World!</h1>
+  <div>
+    <h1>Hello, World!</h1>
+    <p>Items: {{ url }} {{ item }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios  }) {
+    // @todo Avoid this magic, probably by deactivating server side generation
+    let url = "/ppl-dev/api/"
+    if (typeof window === 'undefined') {
+      url = "http://backend:8000/"
+    }
+    url += "items/42"
+
+    const item = await $axios.$get(url)
+    return { item, url }
+  }
+}
+</script>
