8b8192a95a8545fde8330a7967bbcb1807428fee
Vincent Jacques <vincent@vincent-jacques.net>
2022-06-09 14:04:26 +0200

    Get started with Bootstrap

--- ui/frontend/layouts/default.vue
+++ ui/frontend/layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <b-container fluid="md">
+    <p>Bootstrap size (debug) =
+      <span class="d-sm-none">XS <i>i.e.</i> width &lt; 576px</span>
+      <span class="d-none d-sm-inline d-md-none">SM <i>i.e.</i> 576px &le; width &lt; 768px</span>
+      <span class="d-none d-md-inline d-lg-none">MD <i>i.e.</i> 768px &le; width &lt; 992px</span>
+      <span class="d-none d-lg-inline d-xl-none">LG <i>i.e.</i> 992px &le; width &lt; 1200px</span>
+      <span class="d-none d-xl-inline">XL <i>i.e.</i> 1200px &le; width</span>
+    </p>
     <h1><NuxtLink :to="{name: 'index'}">Parallel preference learning Demo</NuxtLink></h1>
     <Nuxt></Nuxt>
-  </div>
+  </b-container>
 </template>
