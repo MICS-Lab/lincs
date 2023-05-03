38e91f903500692c20a95b2b394b575a478d804f
Vincent Jacques <vincent@vincent-jacques.net>
2023-05-03 07:22:15 +0000

    Fix indentation

--- plad/libplad/libplad_module.cpp
+++ plad/libplad/libplad_module.cpp
@@ -12,44 +12,46 @@
 namespace bp = boost::python;
 
 namespace {
-  class PythonOutputDevice : public boost::iostreams::sink {
-   public:
 
-    explicit PythonOutputDevice(boost::python::object out_file_) : out_file(out_file_) {}
+class PythonOutputDevice : public boost::iostreams::sink {
+  public:
 
-    std::streamsize write(const char* s, std::streamsize n) {
-      out_file.attr("write")(std::string(s, n));
-      return n;
-    }
+  explicit PythonOutputDevice(boost::python::object out_file_) : out_file(out_file_) {}
 
-   private:
-    boost::python::object out_file;
-  };
-
-  void dump_domain(const plad::Domain& domain, bp::object& out_file) {
-    boost::iostreams::stream<PythonOutputDevice> out_stream(out_file);
-    domain.dump(out_stream);
+  std::streamsize write(const char* s, std::streamsize n) {
+    out_file.attr("write")(std::string(s, n));
+    return n;
   }
 
-  class PythonInputDevice : public boost::iostreams::source {
-   public:
+  private:
+  boost::python::object out_file;
+};
 
-    explicit PythonInputDevice(boost::python::object in_file_) : in_file(in_file_) {}
+void dump_domain(const plad::Domain& domain, bp::object& out_file) {
+  boost::iostreams::stream<PythonOutputDevice> out_stream(out_file);
+  domain.dump(out_stream);
+}
 
-    std::streamsize read(char* s, std::streamsize n) {
-      std::string str = bp::extract<std::string>(in_file.attr("read")(n));
-      std::copy(str.begin(), str.end(), s);
-      return str.size();
-    }
+class PythonInputDevice : public boost::iostreams::source {
+  public:
 
-   private:
-    boost::python::object in_file;
-  };
+  explicit PythonInputDevice(boost::python::object in_file_) : in_file(in_file_) {}
 
-  plad::Domain load_domain(bp::object& in_file) {
-    boost::iostreams::stream<PythonInputDevice> in_stream(in_file);
-    return plad::Domain::load(in_stream);
+  std::streamsize read(char* s, std::streamsize n) {
+    std::string str = bp::extract<std::string>(in_file.attr("read")(n));
+    std::copy(str.begin(), str.end(), s);
+    return str.size();
   }
+
+  private:
+  boost::python::object in_file;
+};
+
+plad::Domain load_domain(bp::object& in_file) {
+  boost::iostreams::stream<PythonInputDevice> in_stream(in_file);
+  return plad::Domain::load(in_stream);
+}
+
 }  // namespace
 
 BOOST_PYTHON_MODULE(libplad) {
