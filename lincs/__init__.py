b2d52b41e00b354ce0aa4c48bc23da0a14d22eb0
Vincent Jacques <vincent@vincent-jacques.net>
2023-07-04 15:06:25 +0000

    Comment

--- lincs/__init__.py
+++ lincs/__init__.py
@@ -1,14 +1,19 @@
 # Copyright 2023 Vincent Jacques
 
+# I/O, generation, classification
 from liblincs import ValueType, CategoryCorrelation, Criterion, Category, Problem, PROBLEM_JSON_SCHEMA, load_problem, generate_classification_problem
 from liblincs import SufficientCoalitionsKind, SufficientCoalitions, Boundary, Model, MODEL_JSON_SCHEMA, load_model, generate_mrsort_classification_model
 from liblincs import Alternative, Alternatives, load_alternatives, generate_classified_alternatives, misclassify_alternatives, classify_alternatives
+
+# Learning - weights-profiles-breed
 from liblincs import WeightsProfilesBreedMrSortLearning
 from liblincs import InitializeProfilesForProbabilisticMaximalDiscriminationPowerPerCriterion
 from liblincs import OptimizeWeightsUsingGlop, OptimizeWeightsUsingAlglib
 from liblincs import ImproveProfilesWithAccuracyHeuristicOnCpu, ImproveProfilesWithAccuracyHeuristicOnGpu
 from liblincs import ReinitializeLeastAccurate
 from liblincs import TerminateAtAccuracy
+
+# Learning - SAT by coalitions
 from liblincs import SatCoalitionUcncsLearningUsingMinisat, SatCoalitionUcncsLearningUsingEvalmaxsat
 
 # @todo Accept learning and training set as Pandas DataFrame?
