ae4ad938328707af859e8a90b87eae4bf584ea8a
Vincent Jacques <vincent@vincent-jacques.net>
2023-08-30 06:42:11 +0000

    Classify using decreasing criteria

--- lincs/liblincs/classification.cpp
+++ lincs/liblincs/classification.cpp
@@ -15,28 +15,28 @@ bool is_good_enough(const Problem& problem, const Model::Boundary& boundary, con
 
   switch (boundary.sufficient_coalitions.kind) {
     case SufficientCoalitions::Kind::weights: {
-      float weight_at_or_above_profile = 0;
+      float weight_at_or_better_than_profile = 0;
       for (unsigned criterion_index = 0; criterion_index != criteria_count; ++criterion_index) {
         const float alternative_value = alternative.profile[criterion_index];
         const float profile_value = boundary.profile[criterion_index];
-        if (alternative_value >= profile_value) {
-          weight_at_or_above_profile += boundary.sufficient_coalitions.criterion_weights[criterion_index];
+        if (problem.criteria[criterion_index].better_or_equal(alternative_value, profile_value)) {
+          weight_at_or_better_than_profile += boundary.sufficient_coalitions.criterion_weights[criterion_index];
         }
       }
-      return weight_at_or_above_profile >= 1.f;
+      return weight_at_or_better_than_profile >= 1.f;
     }
     case SufficientCoalitions::Kind::roots: {
-      boost::dynamic_bitset<> at_or_above_profile(criteria_count);
+      boost::dynamic_bitset<> at_or_better_than_profile(criteria_count);
       for (unsigned criterion_index = 0; criterion_index != criteria_count; ++criterion_index) {
         const float alternative_value = alternative.profile[criterion_index];
         const float profile_value = boundary.profile[criterion_index];
-        if (alternative_value >= profile_value) {
-          at_or_above_profile[criterion_index] = true;
+        if (problem.criteria[criterion_index].better_or_equal(alternative_value, profile_value)) {
+          at_or_better_than_profile[criterion_index] = true;
         }
       }
 
       for (boost::dynamic_bitset<> root: boundary.sufficient_coalitions.upset_roots) {
-        if ((at_or_above_profile & root) == root) {
+        if ((at_or_better_than_profile & root) == root) {
           return true;
         }
       }
@@ -91,13 +91,13 @@ TEST_CASE("Basic classification using weights") {
 
   Alternatives alternatives{problem, {
     {"A", {0.49, 0.49, 0.49}, std::nullopt},
-    {"A", {0.5, 0.49, 0.49}, std::nullopt},
-    {"A", {0.49, 0.5, 0.49}, std::nullopt},
-    {"A", {0.49, 0.49, 0.5}, std::nullopt},
-    {"A", {0.49, 0.5, 0.5}, std::nullopt},
-    {"A", {0.5, 0.49, 0.5}, std::nullopt},
-    {"A", {0.5, 0.5, 0.49}, std::nullopt},
-    {"A", {0.5, 0.5, 0.5}, std::nullopt},
+    {"A", {0.50, 0.49, 0.49}, std::nullopt},
+    {"A", {0.49, 0.50, 0.49}, std::nullopt},
+    {"A", {0.49, 0.49, 0.50}, std::nullopt},
+    {"A", {0.49, 0.50, 0.50}, std::nullopt},
+    {"A", {0.50, 0.49, 0.50}, std::nullopt},
+    {"A", {0.50, 0.50, 0.49}, std::nullopt},
+    {"A", {0.50, 0.50, 0.50}, std::nullopt},
   }};
 
   auto result = classify_alternatives(problem, model, &alternatives);
@@ -131,13 +131,13 @@ TEST_CASE("Basic classification using upset roots") {
 
   Alternatives alternatives{problem, {
     {"A", {0.49, 0.49, 0.49}, std::nullopt},
-    {"A", {0.5, 0.49, 0.49}, std::nullopt},
-    {"A", {0.49, 0.5, 0.49}, std::nullopt},
-    {"A", {0.49, 0.49, 0.5}, std::nullopt},
-    {"A", {0.49, 0.5, 0.5}, std::nullopt},
-    {"A", {0.5, 0.49, 0.5}, std::nullopt},
-    {"A", {0.5, 0.5, 0.49}, std::nullopt},
-    {"A", {0.5, 0.5, 0.5}, std::nullopt},
+    {"A", {0.50, 0.49, 0.49}, std::nullopt},
+    {"A", {0.49, 0.50, 0.49}, std::nullopt},
+    {"A", {0.49, 0.49, 0.50}, std::nullopt},
+    {"A", {0.49, 0.50, 0.50}, std::nullopt},
+    {"A", {0.50, 0.49, 0.50}, std::nullopt},
+    {"A", {0.50, 0.50, 0.49}, std::nullopt},
+    {"A", {0.50, 0.50, 0.50}, std::nullopt},
   }};
 
   auto result = classify_alternatives(problem, model, &alternatives);
@@ -150,6 +150,46 @@ TEST_CASE("Basic classification using upset roots") {
   CHECK(alternatives.alternatives[5].category_index == 1);
   CHECK(alternatives.alternatives[6].category_index == 0);
   CHECK(alternatives.alternatives[7].category_index == 1);
+  CHECK(result.unchanged == 0);
+  CHECK(result.changed == 8);
+}
+
+TEST_CASE("Classification with decreasing criteria") {
+  Problem problem{
+    {
+      {"Criterion 1", Criterion::ValueType::real, Criterion::CategoryCorrelation::decreasing, 0, 1},
+      {"Criterion 2", Criterion::ValueType::real, Criterion::CategoryCorrelation::decreasing, 0, 1},
+      {"Criterion 3", Criterion::ValueType::real, Criterion::CategoryCorrelation::decreasing, 0, 1},
+    },
+    {{"Category 1"}, {"Category 2"}},
+  };
+
+  Model model{
+    problem,
+    {{{0.5, 0.5, 0.5}, {SufficientCoalitions::weights, {0.3, 0.6, 0.8}}}},
+  };
+
+  Alternatives alternatives{problem, {
+    {"A", {0.50, 0.50, 0.50}, std::nullopt},
+    {"A", {0.51, 0.50, 0.50}, std::nullopt},
+    {"A", {0.50, 0.51, 0.50}, std::nullopt},
+    {"A", {0.50, 0.50, 0.51}, std::nullopt},
+    {"A", {0.50, 0.51, 0.51}, std::nullopt},
+    {"A", {0.51, 0.50, 0.51}, std::nullopt},
+    {"A", {0.51, 0.51, 0.50}, std::nullopt},
+    {"A", {0.51, 0.51, 0.51}, std::nullopt},
+  }};
+
+  auto result = classify_alternatives(problem, model, &alternatives);
+
+  CHECK(alternatives.alternatives[0].category_index == 1);
+  CHECK(alternatives.alternatives[1].category_index == 1);
+  CHECK(alternatives.alternatives[2].category_index == 1);
+  CHECK(alternatives.alternatives[3].category_index == 0);
+  CHECK(alternatives.alternatives[4].category_index == 0);
+  CHECK(alternatives.alternatives[5].category_index == 0);
+  CHECK(alternatives.alternatives[6].category_index == 0);
+  CHECK(alternatives.alternatives[7].category_index == 0);
   CHECK(result.unchanged == 0);
   CHECK(result.changed == 8);
 }
