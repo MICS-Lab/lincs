0667693b5d592a03bfd0be6cc7baf0407a41f86d
Vincent Jacques <vincent@vincent-jacques.net>
2024-02-08 16:52:09 +0000

    Publish version 1.1.0a16

--- lincs/__init__.py
+++ lincs/__init__.py
@@ -18,7 +18,7 @@ from liblincs import UniformRandomBitsGenerator
 from . import classification
 
 # General information
-__version__ = "1.1.0a16.dev0"
+__version__ = "1.1.0a16"
 has_gpu = hasattr(classification, "ImproveProfilesWithAccuracyHeuristicOnGpu")
 
 try:
